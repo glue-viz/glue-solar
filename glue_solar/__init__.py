c3d547bad5a3c79dcee05b9456a85c5d32bc66a1
mbobra <mbobra@stanford.edu>
2019-08-01 15:03:30 -0400

    moved imports to the top of the file

--- glue_solar/__init__.py
+++ glue_solar/__init__.py
@@ -1,15 +1,15 @@
 from pkg_resources import get_distribution, DistributionNotFound
 from sunpy.cm import cmlist
-
+from glue.viewers.image.qt import ImageViewer
+from glue_solar.pixel_extraction import PixelExtractionTool  # noqa
+from glue.config import colormaps
+    
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     pass
 
 def setup():
-    from glue.viewers.image.qt import ImageViewer
-    from glue_solar.pixel_extraction import PixelExtractionTool  # noqa
     ImageViewer.tools.append('solar:pixel_extraction')
-    from glue.config import colormaps
     for name, ctable in cmlist.items():
         colormaps.add(name, ctable)
