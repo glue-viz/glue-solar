f3f7aae07c4911f1f6ed08e4a6cc1ba855fb2dad
Kris Stern <krisastern@gmail.com>
2020-08-25 02:09:17 +0800

    Remove \ from line break

--- glue_solar/tests/test_iris_reader.py
+++ glue_solar/tests/test_iris_reader.py
@@ -14,7 +14,7 @@ def test_iris_raster_reader():
     To test whether the read_iris_raster is returning a list or a str
     """
 
-    with mock.patch('glue_solar.instruments.iris', return_value=np.zeros((42, 42, 42))) \
-            as mock_iris:
+    with mock.patch('glue_solar.instruments.iris',
+                    return_value=np.zeros((42, 42, 42))) as mock_iris:
         raster_data = mock_iris.read_iris_raster(os.path.join(DATA, 'iris_test_data'))
         assert not isinstance(raster_data, list)
